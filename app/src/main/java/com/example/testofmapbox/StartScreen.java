d45712d1c69f0958967d5559a5b8fd17187baf1e
ethernet389 <etheret389@gmail.com>
2023-03-26 14:44:20 +0800

    commit

--- app/src/main/java/com/example/testofmapbox/StartScreen.java
+++ app/src/main/java/com/example/testofmapbox/StartScreen.java
@@ -3,14 +3,16 @@ package com.example.testofmapbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class StartScreen extends AppCompatActivity {
     ListView listView;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +26,13 @@ public class StartScreen extends AppCompatActivity {
         );
 
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(StartScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
