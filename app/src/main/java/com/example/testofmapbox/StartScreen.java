8141ad5fa99282ff2645ed32f4b0ed45d8011035
ethernet389 <etheret389@gmail.com>
2023-03-26 14:34:08 +0800

    next commit

--- app/src/main/java/com/example/testofmapbox/StartScreen.java
+++ app/src/main/java/com/example/testofmapbox/StartScreen.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class StartScreen extends AppCompatActivity {
@@ -17,5 +18,11 @@ public class StartScreen extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.university);
         String[] university = getResources().getStringArray(R.array.university);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, university
+        );
+
+        listView.setAdapter(arrayAdapter);
     }
 }
